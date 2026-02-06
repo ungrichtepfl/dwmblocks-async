223c956a983686cf7a9114c85bd07f8a0f3a67d3
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-06 23:51:38 +0100

    feat: config status to display.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- config.h
+++ config.h
@@ -11,22 +11,22 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 0
+#define LEADING_DELIMITER 1
 
 // Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 0
+#define TRAILING_DELIMITER 1
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+// Stolen from https://github.com/UtkarshVerma/dotfiles/tree/main/.local/bin/statusbar
+#define BLOCKS(X)              \
+    X("", "sb-music",    0, 1) \
+    X("", "sb-disk",     5, 2) \
+    X("", "sb-memory",   2, 3) \
+    X("", "sb-loadavg",  2, 4) \
+    X("", "sb-mic",      0, 5) \
+    X("", "sb-record",   0, 6) \
+    X("", "sb-volume",   0, 7) \
+    X("", "sb-battery",  5, 8) \
+    X("", "sb-date",     1, 9)
 
 #endif  // CONFIG_H
