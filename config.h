31f331fd69c1c8e39f0da99663bf5d71509f862c
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-08 00:21:42 +0100

    feat: Handle delimiters in status scripts.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER ""
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
@@ -11,22 +11,23 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 1
+#define LEADING_DELIMITER 0
 
 // Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 1
+#define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 // Stolen from https://github.com/UtkarshVerma/dotfiles/tree/main/.local/bin/statusbar
-#define BLOCKS(X)              \
-    X("", "sb-music",    0, 1) \
-    X("", "sb-disk",     5, 2) \
-    X("", "sb-memory",   2, 3) \
-    X("", "sb-loadavg",  2, 4) \
-    X("", "sb-mic",      0, 5) \
-    X("", "sb-record",   0, 6) \
-    X("", "sb-volume",   0, 7) \
-    X("", "sb-battery",  5, 8) \
-    X("", "sb-date",     1, 9)
+#define BLOCKS(X)                 \
+    X("", "sb-music",     0,  11) \
+    X("", "sb-network",   4,  10) \
+    X("", "sb-disk",      3,   8) \
+    X("", "sb-memory",    2,   7) \
+    X("", "sb-loadavg",   2,   6) \
+    X("", "sb-mic",       0,   5) \
+    X("", "sb-record",    0,   4) \
+    X("", "sb-volume",    0,   3) \
+    X("", "sb-battery",   5,   2) \
+    X("", "sb-date",      1,   1)
 
 #endif  // CONFIG_H
